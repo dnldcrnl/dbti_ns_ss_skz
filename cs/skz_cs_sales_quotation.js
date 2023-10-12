2b9b1a1f370c0e5121759214855edd4a66a0f164
rckyrd <ricky.tbsi@gmail.com>
2023-10-12 10:52:20 +0800

    partial modification cs_sq

--- cs/skz_cs_sales_quotation.js
+++ cs/skz_cs_sales_quotation.js
@@ -27,9 +27,39 @@ define(['N/record'],
             }
         }
 
+        function sublistChanged(context) {
+
+            var currentRecord = context.currentRecord;
+            var sublistName = context.sublistId;
+
+            if (sublistName === 'item') {
+
+                var unit_price = currentRecord.getCurrentSublistValue({
+                    sublistId: sublistName,
+                    fieldId: 'rate',
+                });
+
+                var discount_rate = currentRecord.getCurrentSublistValue({
+                    sublistId: sublistName,
+                    fieldId: 'custcol_discountrate',
+                });
+
+                // console.log(currentRecord.getCurrentSublistValue({
+                //     sublistId: sublistName,
+                //     fieldId: 'amount',
+                // }));
+            }
+            // currentRecord.setValue({
+            //     fieldId: 'memo',
+            //     value: 'Total has changed to ' + currentRecord.getValue({ fieldId: 'total' }) +
+            //         'with operation: ' + op
+            // });
+        }
+
         return {
             pageInit: pageInit,
             fieldChanged: fieldChanged,
+            sublistChanged: sublistChanged,
         };
 
     });
